bf511dce78d56e66f6ba2588009e368096a81676
KooSeongMin <ababqq@naver.com>
2022-03-10 18:02:05 +0900

    004|[feat] ADD Weather feature / ui

---
+++ app/src/main/java/com/ababqq/motov_test_android/viewmodels/WeatherViewModel.java
@@ -0,0 +1,7 @@
+package com.ababqq.motov_test_android.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class WeatherViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
